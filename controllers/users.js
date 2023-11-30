9129f98e7c4bdfd226251c32dc344903782aaab0
Georgy Akopov <akopovzh@yandex.ru>
2023-11-30 17:10:29 +0300

    jwt

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET = 'SECRET_KEY' } = process.env;
+const JWT_SECRET = 'SECRET_KEY';
 
 const {
   HTTP_STATUS_OK,
