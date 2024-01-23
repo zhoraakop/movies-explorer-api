035e54ab83637a3e920edb059eafe87103dff5d9
Georgy Akopov <akopovzh@yandex.ru>
2024-01-23 15:26:52 +0300

    add/family

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const {
 mongoose
   .connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
     useNewUrlParser: true,
+    family: 4,
   }).then(() => console.log('CONNECTED TO MONGODB'));
 
 const app = express();
